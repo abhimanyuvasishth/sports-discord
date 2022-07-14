f47dafac624a9ee206cf6d1a34ab8b495a6bffde
abhimanyuvasishth <abhimanyu.vasishth@gmail.com>
2022-07-14 12:45:11 -0500

    alembic init

---
+++ alembic/versions/17dad96dc3c9_create_tournament_table.py
@@ -0,0 +1,24 @@
+"""create tournament table
+
+Revision ID: 17dad96dc3c9
+Revises: 
+Create Date: 2022-07-14 12:37:24.557651
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '17dad96dc3c9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
