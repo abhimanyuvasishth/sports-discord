c910380aa45e32529b24a2bcc42c6ca544ba8033
abhimanyuvasishth <abhimanyu.vasishth@gmail.com>
2022-08-27 10:57:10 -0700

    added configs as code

--- scripts/insert_data.py
+++ scripts/insert_data.py
@@ -5,64 +5,12 @@ from dateutil.parser import parse
 from dotenv import load_dotenv
 from sports_discord.models import (Match, MatchPlayer, Player, Team,
                                    Tournament, UserTeam, UserTeamPlayer)
+from sports_discord.google_sheet import get_sheet
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 engine = create_engine(getenv('POSTGRES_URL'))
-
-
-# From scorecard autopopulater scraper
-MATCH_CONFIGS = [
-    {
-        'team_1': 'Chennai Super Kings',
-        'team_1_num': 1,
-        'team_2': 'Delhi Capitals',
-        'team_2_num': 2,
-        'start_timestamp': '2022-08-29T00:00:00.000Z',
-        'object_id': 1312200
-    },
-]
-
-# Post Auction scrape from sheet
-PLAYER_CONFIGS = [
-    {
-        'name': 'Ambati Rayudu',
-        'team_name': 'Chennai Super Kings',
-        'user_team_name': 'Namit',
-        'pool': 'A'
-    },
-    {
-        'name': 'Maheesh Theekshana',
-        'team_name': 'Chennai Super Kings',
-        'user_team_name': 'Shiv, Aryaman',
-        'pool': 'B'
-    },
-    {
-        'name': 'Robin Uthappa',
-        'team_name': 'Chennai Super Kings',
-        'user_team_name': 'Desai, Kush, Naman',
-        'pool': 'C'
-    },
-    {
-        'name': 'Anrich Nortje',
-        'team_name': 'Delhi Capitals',
-        'user_team_name': 'Paro, Rahul, Taro',
-        'pool': 'A'
-    },
-    {
-        'name': 'Rovman Powell',
-        'team_name': 'Delhi Capitals',
-        'user_team_name': 'Shiv, Aryaman',
-        'pool': 'B'
-    },
-    {
-        'name': 'Vicky Ostwal',
-        'team_name': 'Delhi Capitals',
-        'user_team_name': None,
-        'pool': 'B'
-    },
-]
 
 
 def insert_tournament(tournament_config):
@@ -82,21 +30,33 @@ def insert_teams(team_configs):
         session.bulk_save_objects([Team(**config, tournament_id=1) for config in team_configs])
 
 
-def insert_players():
+def create_player_configs():
+    player_configs = []
+    sheet = get_sheet('Asia Cup 2022', 'Bidding Page')
+    rows = sheet.get_all_values()
+    for row in rows[20:64]:
+        player_configs.append({
+            'name': row[1],
+            'team_name': row[2],
+            'user_team_name': row[8]
+        })
+    return player_configs
+
+
+def insert_players(player_configs):
     with sessionmaker(engine, autocommit=True).begin() as session:
-        for config in PLAYER_CONFIGS:
+        for config in player_configs:
             team_id = session.query(Team).filter(Team.name == config['team_name']).first().id
             player = Player(
                 name=config['name'],
                 team_id=team_id,
-                # pool=PlayerPool[config['pool']]
             )
             session.add(player)
 
 
-def insert_user_team_players():
+def insert_user_team_players(player_configs):
     with sessionmaker(engine, autocommit=True).begin() as session:
-        for config in PLAYER_CONFIGS:
+        for config in player_configs:
             user_team = session.query(UserTeam).filter(
                 UserTeam.name == config['user_team_name']
             ).first()
@@ -106,9 +66,9 @@ def insert_user_team_players():
             session.add(UserTeamPlayer(player_id=player_id, user_team_id=user_team.id))
 
 
-def insert_matches():
+def insert_matches(match_configs):
     with sessionmaker(engine, autocommit=True).begin() as session:
-        for config in MATCH_CONFIGS:
+        for config in match_configs:
             match_1 = Match(
                 team_id=session.query(Team).filter(Team.name == config['team_1']).first().id,
                 external_id=config['object_id'],
@@ -124,12 +84,12 @@ def insert_matches():
             session.bulk_save_objects([match_1, match_2])
 
 
-def insert_match_players():
+def insert_match_players(player_configs, match_configs):
     with sessionmaker(engine, autocommit=True).begin() as session:
-        for match_config in MATCH_CONFIGS:
+        for match_config in match_configs:
             team = session.query(Team).filter(Team.name == match_config['team_1']).first()
             match = session.query(Match).filter(Match.team_id == team.id).first()
-            for player_config in PLAYER_CONFIGS:
+            for player_config in player_configs:
                 if team.name == player_config['team_name']:
                     player = session.query(Player).filter(
                         Player.name == player_config['name']
@@ -152,7 +112,9 @@ if __name__ == '__main__':
     insert_tournament(configs['tournament'])
     insert_user_teams(configs['user_teams'])
     insert_teams(configs['teams'])
-    # insert_players()
-    # insert_user_team_players()
-    # insert_matches()
-    # insert_match_players()
+
+    player_configs = create_player_configs()
+    insert_players(player_configs)
+    insert_user_team_players(player_configs)
+    insert_matches(configs['matches'])
+    insert_match_players(player_configs, configs['matches'])
