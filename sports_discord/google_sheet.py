0113007696621dbae0638abb826f652404d4cb27
abhimanyuvasishth <abhimanyu.vasishth@gmail.com>
2022-08-27 14:52:51 -0700

    sheet credentials added as json

--- sports_discord/google_sheet.py
+++ sports_discord/google_sheet.py
@@ -1,7 +1,13 @@
 from functools import cache
 
 import gspread
+import json
+import os
+from dotenv import load_dotenv
 from oauth2client.service_account import ServiceAccountCredentials
+
+
+load_dotenv()
 
 
 @cache
@@ -10,10 +16,11 @@ def get_sheet(doc_name, sheet_name):
     return client.open(doc_name).worksheet(sheet_name)
 
 
-def authorize_client(file_name='credentials.json'):
+def authorize_client():
     scopes = [
         'https://www.googleapis.com/auth/drive',
         'https://www.googleapis.com/auth/drive.file'
     ]
-    creds = ServiceAccountCredentials.from_json_keyfile_name(file_name, scopes)
+    parsed_dict = json.loads(os.getenv('GOOGLE_SHEETS_CREDENTIALS'))
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(parsed_dict, scopes)
     return gspread.authorize(creds)
