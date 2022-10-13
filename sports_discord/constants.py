35a79f270f28af73431cce083542a20a059e8439
abhimanyuvasishth <abhimanyu.vasishth@gmail.com>
2022-10-13 11:53:56 -0700

    updated tournament

--- sports_discord/constants.py
+++ sports_discord/constants.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-DOC_NAME = 'Asia Cup 2022'
+DOC_NAME = 'World Cup 2022'
 BIDDING_SHEET_NAME = 'Bidding Page'
 POINTS_SHEET_NAME = 'Points Worksheet'
 TEAM_POINTS_SHEET_NAME = 'Team Points'
