9016b3aec6a07e10d4cad7b347fac1001da5056c
abhimanyuvasishth <abhimanyu.vasishth@gmail.com>
2022-11-07 10:17:39 -0800

    rating emoji test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,13 @@
 from sports_discord import utils
 
 
-def test_emoji_from_number():
+def test_get_emoji_from_number():
     assert utils.get_emoji_from_number(100) == ':one::zero::zero:'
+
+
+def test_get_rating_emoji():
+    assert utils.get_rating_emoji(1, 100) == ':fire:'
+    assert utils.get_rating_emoji(23, 100) == ':grinning:'
+    assert utils.get_rating_emoji(34, 100) == ':slight_smile:'
+    assert utils.get_rating_emoji(59, 100) == ':neutral_face:'
+    assert utils.get_rating_emoji(95, 100) == ':lemon:'
