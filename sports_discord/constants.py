a4b3529e431525ef99e1b5f52bc48d6c30ee4b48
abhimanyuvasishth <abhimanyu.vasishth@gmail.com>
2023-04-01 04:28:44 -0700

    points field

--- sports_discord/constants.py
+++ sports_discord/constants.py
@@ -5,7 +5,7 @@ BIDDING_SHEET_NAME = 'Bidding Page'
 POINTS_SHEET_NAME = 'Points Worksheet'
 TEAM_POINTS_SHEET_NAME = 'Team Points'
 NOT_ON_A_TEAM = 'Not yet a part of any teams for this auction/draft. Please join a team first.'
-NUMBER_OF_FIELDS = 3
+NUMBER_OF_FIELDS = 15
 
 
 class Pool(Enum):
