3d1efd7ed5023feb768c6e96b190bc1ca070b301
abhimanyuvasishth <abhimanyu.vasishth@gmail.com>
2024-05-31 18:13:52 -0700

    setup for new sheet

--- sports_discord/constants.py
+++ sports_discord/constants.py
@@ -24,8 +24,8 @@ class Position(Enum):
 
 class SheetCols(Enum):
     NAME_COL = 0
-    POINTS_COL = 5
-    RAW_POINTS_COL = 261
+    POINTS_COL = 4
+    RAW_POINTS_COL = 260
     POINTS_OWNER_COL = 3
     BIDDING_OWNER_COL = 9
     ADJUSTED_POINTS_COL = 22
