a4adbc9b4e1a4672b21d75f9e4899f9c6784b354
abhimanyuvasishth <abhimanyu.vasishth@gmail.com>
2025-03-22 23:30:08 -0700

    emoji nickname support

--- discord_bot.py
+++ discord_bot.py
@@ -292,12 +292,13 @@ async def nickname(context, *args):
 
     split_index = args.index('as')
     player_name_raw = ' '.join(args[:split_index])
-    nickname = ' '.join(args[split_index + 1:])
+    nickname_raw = ' '.join(args[split_index + 1:])
 
-    if not player_name_raw or not nickname:
+    if not player_name_raw or not nickname_raw:
         return await context.reply("Missing player name or nickname")
 
     player_name = emoji.demojize(player_name_raw).replace(':', '')
+    nickname = emoji.demojize(player_name_raw)
 
     players = db_utils.get_player_out(player_name, role_id)
     if len(players) == 0:
